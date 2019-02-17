a309981861adcee287f3d843efe487c35cd2d1ba
FABIEN <goodnightkiss1972@gmail.com>
2019-02-17 05:14:00 +0100

    tp4 fin exercice 4

--- CPEI/tp4/Exercice4.c
+++ CPEI/tp4/Exercice4.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 
-int pgcd(int a, int b);
+int pgcd_iteration(int a, int b);
+int pgcd_recursion(int a, int b);
 
 int main()
 {
     int a, b;
     printf("Entrez deux nombres entiers svp :\n");
     scanf("%d %d", &a, &b);
-    printf("Technique iteration: pgcd(%d,%d) = %d\n", a, b, pgcd(a, b));
+    printf("Technique iteration: pgcd(%d,%d) = %d\n", a, b, pgcd_iteration(a, b));
+    printf("Technique recursion: pgcd(%d,%d) = %d\n", a, b, pgcd_recursion(a, b));
     return 0;
 }
 
-int pgcd(int a, int b)
+int pgcd_iteration(int a, int b)
 {
     int minab, i, pgcd;
     if (a < 1 || b < 1)
@@ -37,4 +39,27 @@ int pgcd(int a, int b)
         }
     }
     return pgcd;
+}
+
+int pgcd_recursion(int a, int b)
+{
+    int reste;
+    if (a < 1 || b < 1)
+    {
+        printf("pgcd demande deux entiers strictement superieurs a 1.\n");
+        return -1;
+    }
+    if (a == b)
+    {
+        return a;
+    }
+    reste = a % b;
+    printf("reste de %d %% %d = %d\n", a, b, reste);
+    if (reste == 0) {
+        return b;
+    }
+    else
+    {
+        return pgcd_recursion(b, reste);
+    }
 }
