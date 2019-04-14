4e14aa751e73c37ce61f365eaad6c655be41cbcc
goodnightkiss1972 <goodnightkiss1972@gmail.com>
2019-04-14 18:56:57 +0200

    listes chainees exo2 partiel

--- CPEI/tp9/tp9.c
+++ CPEI/tp9/tp9.c
@@ -25,20 +25,29 @@ list *new_list(int array[], int len)
 
 /* Exercice 1 */
 void affiche_liste(list *l);
-int taille_list(list *l);
+int taille_liste(list *l);
 list *renverse_liste(list *l);
+
+/* Exercice 2 */
+int valeur_a_la_position_list(list *l, int i);
+int position_de_la_premiere_valeur_list(list *l, int valeur);
 
 int main()
 {
-
-    int tableau[10] = {5, 6, 7, 2, 3, 10, 4, 1, 0, -1};
+    int tableau[10] = {5, 6, 7, 2, 3, 10, 4, 1, 0, 1};
 
     list *maliste = new_list(tableau, 10);
     affiche_liste(maliste);
-    printf("La liste contient %d elements.\n", taille_list(maliste));
+    printf("La liste contient %d elements.\n", taille_liste(maliste));
 
     list *malisteinversee = renverse_liste(maliste);
     affiche_liste(malisteinversee);
+
+    printf("L'element 3 de la premiere liste est : %d\n", valeur_a_la_position_list(maliste, 3));
+    printf("L'element 15 de la premiere liste est : %d\n", valeur_a_la_position_list(maliste, 15));
+
+    printf("La premiere valeur 1 est a la position : %d\n", position_de_la_premiere_valeur_list(maliste, 1));
+    printf("La premiere valeur 99 est a la position : %d\n", position_de_la_premiere_valeur_list(maliste, 99));
 }
 
 void affiche_liste(list *l)
@@ -51,7 +60,7 @@ void affiche_liste(list *l)
     printf("\n");
 }
 
-int taille_list(list *l)
+int taille_liste(list *l)
 {
     int taille = 0;
     while (l != NULL)
@@ -66,7 +75,7 @@ list *renverse_liste(list *l)
 {
     // calculer la taille de la liste, puis mettre ses elements a l'envers dans un tableau
     // puis creer la liste a partir de ce tableau avec la fonction du prof.
-    int taille = taille_list(l);
+    int taille = taille_liste(l);
     if (taille == 0)
     {
         printf("ERREUR: la liste est vide.\n");
@@ -81,4 +90,42 @@ list *renverse_liste(list *l)
     }
     list *retour = new_list(tableau, taille);
     return retour;
+}
+
+int valeur_a_la_position_list(list *l, int i)
+{
+    int indice = 0;
+    while (l != NULL) // on balaye la liste
+    {
+        if (indice == i) // on a trouve l'element i
+        {
+            return l->head;
+        }
+        else // on avance
+        {
+            l = l->queue;
+            indice++;
+        }
+    }
+    // remarque au dernier coup on a pu avancer dans le vide et donc le while s'interrompt.
+    return -1; // on est pas arrive a i car la liste s'est terminee avant
+}
+
+int position_de_la_premiere_valeur_list(list *l, int valeur)
+{
+    int indice = 0;
+    while (l != NULL) // on balaye la liste
+    {
+        if (l->head == valeur) // on a trouve l'element i
+        {
+            return indice;
+        }
+        else // on avance
+        {
+            l = l->queue;
+            indice++;
+        }
+    }
+    // remarque au dernier coup on a pu avancer dans le vide et donc le while s'interrompt.
+    return -1; // on est pas arrive a la valeur car la liste s'est terminee avant
 }
