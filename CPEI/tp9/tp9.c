20fdd0e6b396ec4f55a9cc50a359ed30401a26d6
goodnightkiss1972 <goodnightkiss1972@gmail.com>
2019-04-15 09:05:20 +0200

    listes chainees fin du tp9

--- CPEI/tp9/tp9.c
+++ CPEI/tp9/tp9.c
@@ -33,9 +33,14 @@ int valeur_a_la_position_liste(list *l, int i);
 int position_de_la_premiere_valeur_liste(list *l, int valeur);
 int indice_minimum_liste(list *l);
 
+/* Exercice 3 */
+list *echange_dans_liste(list *l, int src, int dest);
+int indice_minimum_apres_liste(list *l, int apres);
+list *trie_liste(list *l);
+
 int main()
 {
-    int tableau[10] = {5, 6, 7, 2, 3, 10, 4, 1, 0, 12};
+    int tableau[10] = {5, 6, 7, 1, 3, 10, 4, 2, 9, 0};
 
     list *maliste = new_list(tableau, 10);
     printf("\n");
@@ -56,6 +61,20 @@ int main()
     affiche_liste(maliste);
 
     printf("\nL'indice du minimum de la liste est : %d\n", indice_minimum_liste(maliste));
+
+    printf("\nAvant echange de 1 vers 3\n");
+    affiche_liste(maliste);
+
+    list *malisteechangee = echange_dans_liste(maliste, 1, 3);
+    printf("\nApres echange de 1 vers 3\n");
+    affiche_liste(malisteechangee);
+
+    printf("\nAvant tri\n");
+    affiche_liste(maliste);
+
+    list *malistetriee = trie_liste(maliste);
+    printf("\nApres tri\n");
+    affiche_liste(malistetriee);
 }
 
 void affiche_liste(list *l)
@@ -86,7 +105,7 @@ list *renverse_liste(list *l)
     int taille = taille_liste(l);
     if (taille == 0)
     {
-        printf("ERREUR: la liste est vide.\n");
+        printf("\nERREUR: la liste est vide.\n");
         return NULL;
     }
     int tableau[taille];
@@ -143,12 +162,12 @@ int indice_minimum_liste(list *l)
     int taille = taille_liste(l);
     if (taille == 0)
     {
-        printf("ERREUR: la liste est vide.\n");
+        printf("\nERREUR: la liste est vide.\n");
         return INT_MIN;
     }
     int indice = 0, indice_minimum = 0;
     int minimum = l->head; // on initialise le minimum avec la premiere valeur
-    while (l != NULL) // on balaye la liste
+    while (l != NULL)      // on balaye la liste
     {
         if (l->head <= minimum) // on a trouve plus petit ou egal
         {
@@ -159,4 +178,92 @@ int indice_minimum_liste(list *l)
         indice++;
     }
     return indice_minimum;
+}
+
+list *echange_dans_liste(list *l, int src, int dest)
+{
+    int taille = taille_liste(l);
+    if (taille == 0)
+    {
+        printf("\nERREUR: la liste est vide.\n");
+        return NULL;
+    }
+    if (src < 0 || src > taille - 1 || dest < 0 || dest > taille - 1)
+    {
+        printf("\nERREUR: un des indices est en dehors de la liste.\n");
+        return NULL;
+    }
+    int tableau[taille]; // declaration du tableau de travail
+    for (int i = 0; i < taille; i++)
+    {
+        //printf("Boucle %d valeur %d\n", i, l->head);
+        tableau[i] = l->head;
+        l = l->queue;
+    }
+    // echange
+    int temp = tableau[dest];
+    tableau[dest] = tableau[src];
+    tableau[src] = temp;
+
+    list *retour = new_list(tableau, taille);
+    return retour;
+}
+
+int indice_minimum_apres_liste(list *l, int apres)
+{
+    int taille = taille_liste(l);
+    if (taille == 0)
+    {
+        printf("\nERREUR: la liste est vide.\n");
+        return INT_MIN;
+    }
+
+    // on va avancer de "apres" dans la liste avant de commencer
+    // si c'est possible
+    if (apres < 0 || apres > taille - 1)
+    {
+        printf("\nERREUR: indice apres hors limite.\n");
+        return -1;
+    }
+    for (int i = 0; i < apres; i++)
+    {
+        l = l->queue; // on avance
+    }
+
+    int indice = apres, indice_minimum = apres;
+    int minimum = l->head; // on initialise le minimum avec la premiere valeur
+    while (l != NULL)      // on balaye la liste
+    {
+        if (l->head <= minimum) // on a trouve plus petit ou egal
+        {
+            minimum = l->head;
+            indice_minimum = indice;
+        }
+        l = l->queue; // on avance
+        indice++;
+    }
+    return indice_minimum;
+}
+
+list *trie_liste(list *l)
+{
+    int taille = taille_liste(l);
+    if (taille == 0)
+    {
+        printf("\nERREUR: la liste est vide.\n");
+        return NULL;
+    }
+    // tri croissant
+    list *tri = l;
+    for (int i = 0; i < taille; i++)
+    {
+        tri = echange_dans_liste(tri, indice_minimum_apres_liste(tri, i), i);
+    }
+    /*
+    printf("\nIndice minimum apres 0 : %d", indice_minimum_apres_liste(tri, 0));
+    printf("\nIndice minimum apres 4 : %d", indice_minimum_apres_liste(tri, 4));
+    printf("\nIndice minimum apres 9 : %d", indice_minimum_apres_liste(tri, 9));
+    printf("\nIndice minimum apres 10 : %d", indice_minimum_apres_liste(tri, 10));
+*/
+    return tri;
 }
