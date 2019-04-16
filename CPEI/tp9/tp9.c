45a96cbad85e85aabb811d3c76df422b5336c1b0
goodnightkiss1972 <goodnightkiss1972@gmail.com>
2019-04-16 08:46:22 +0200

    listes chainees passage en anglais

--- CPEI/tp9/tp9.c
+++ CPEI/tp9/tp9.c
@@ -24,19 +24,19 @@ list *new_list(int array[], int len)
 }
 
 /* Exercice 1 */
-void affiche_liste(list *l);
-int taille_liste(list *l);
-list *renverse_liste(list *l);
+void display_list(list *l);
+int list_size(list *l);
+list *reverse_list(list *l);
 
 /* Exercice 2 */
-int valeur_a_la_position_liste(list *l, int i);
-int position_de_la_premiere_valeur_liste(list *l, int valeur);
-int indice_minimum_liste(list *l);
+int value_at_position(list *l, int i);
+int position_at_first_value(list *l, int valeur);
+int position_of_minimum(list *l);
 
 /* Exercice 3 */
-list *echange_dans_liste(list *l, int src, int dest);
-int indice_minimum_apres_liste(list *l, int apres);
-list *trie_liste(list *l);
+list *switch_into_list(list *l, int src, int dest);
+int position_of_minimum_after_a_position(list *l, int apres);
+list *order_list(list *l);
 
 /* Supplements */
 list *add_to_list(list *l, int newhead);
@@ -49,70 +49,70 @@ int main()
 
     list *maliste = new_list(tableau, 10);
     printf("\n");
-    affiche_liste(maliste);
-    printf("\nLa liste contient %d elements.\n", taille_liste(maliste));
+    display_list(maliste);
+    printf("\nLa liste contient %d elements.\n", list_size(maliste));
 
-    list *malisteinversee = renverse_liste(maliste);
+    list *malisteinversee = reverse_list(maliste);
     printf("\n");
-    affiche_liste(malisteinversee);
+    display_list(malisteinversee);
 
-    printf("\nL'element 3 de la premiere liste est : %d\n", valeur_a_la_position_liste(maliste, 3));
-    printf("\nL'element 15 de la premiere liste est : %d\n", valeur_a_la_position_liste(maliste, 15));
+    printf("\nL'element 3 de la premiere liste est : %d\n", value_at_position(maliste, 3));
+    printf("\nL'element 15 de la premiere liste est : %d\n", value_at_position(maliste, 15));
 
-    printf("\nLa premiere valeur 1 est a la position : %d\n", position_de_la_premiere_valeur_liste(maliste, 1));
-    printf("\nLa premiere valeur 99 est a la position : %d\n", position_de_la_premiere_valeur_liste(maliste, 99));
+    printf("\nLa premiere valeur 1 est a la position : %d\n", position_at_first_value(maliste, 1));
+    printf("\nLa premiere valeur 99 est a la position : %d\n", position_at_first_value(maliste, 99));
 
     printf("\n");
-    affiche_liste(maliste);
+    display_list(maliste);
 
-    printf("\nL'indice du minimum de la liste est : %d\n", indice_minimum_liste(maliste));
+    printf("\nL'indice du minimum de la liste est : %d\n", position_of_minimum(maliste));
 
     printf("\nAvant echange de 1 vers 3\n");
-    affiche_liste(maliste);
+    display_list(maliste);
 
-    list *malisteechangee = echange_dans_liste(maliste, 1, 3);
+    list *malisteechangee = switch_into_list(maliste, 1, 3);
     printf("\nApres echange de 1 vers 3\n");
-    affiche_liste(malisteechangee);
+    display_list(malisteechangee);
 
     printf("\nAvant tri\n");
-    affiche_liste(maliste);
+    display_list(maliste);
 
-    list *malistetriee = trie_liste(maliste);
+    list *malistetriee = order_list(maliste);
     printf("\nApres tri\n");
-    affiche_liste(malistetriee);
+    display_list(malistetriee);
 
     printf("\nApres ajout de 8 au debut\n");
     malistetriee = add_to_list(malistetriee, 8);
-    affiche_liste(malistetriee);
+    display_list(malistetriee);
 
     printf("\nApres ajout de 11 au debut et tri dans la foulee\n");
-    malistetriee = trie_liste(add_to_list(malistetriee, 11));
-    affiche_liste(malistetriee);
+    malistetriee = order_list(add_to_list(malistetriee, 11));
+    display_list(malistetriee);
 
-    // presque equivalent a "valeur_a_la_position_liste" sauf qu'ici c'est le type list qui est retourne
+    // presque equivalent a "value_at_position" sauf qu'ici c'est le type list qui est retourne
     list *recherche = reach_member_list(maliste, 6);
     printf("\nLa valeur recherchee est %d\n", recherche->head);
-    affiche_liste(maliste);
+    display_list(maliste);
     
     list *malisteajoutee = add_to_list_after_position(maliste, 8, 4);
     printf("\nApres ajout de %d en position %d\n", 8, 4);
-    affiche_liste(malisteajoutee);
+    display_list(malisteajoutee);
 
     malisteajoutee = add_to_list_after_position(malisteajoutee, 15, 0);
     printf("\nApres ajout de %d en position %d\n", 15, 0);
-    affiche_liste(malisteajoutee);
+    display_list(malisteajoutee);
 
     malisteajoutee = add_to_list_after_position(malisteajoutee, 12, 11);
     printf("\nApres ajout de %d en position %d\n", 12, 11);
-    affiche_liste(malisteajoutee);
+    display_list(malisteajoutee);
 
     malisteajoutee = add_to_list_after_position(malisteajoutee, 19, 13);
     printf("\nApres ajout de %d en position %d\n", 19, 13);
-    affiche_liste(malisteajoutee);
+    display_list(malisteajoutee);
 
 }
 
-void affiche_liste(list *l)
+void display_list(list *l)
 {
     while (l != NULL)
     {
@@ -122,7 +122,7 @@ void affiche_liste(list *l)
     printf("\n");
 }
 
-int taille_liste(list *l)
+int list_size(list *l)
 {
     int taille = 0;
     while (l != NULL)
@@ -133,11 +133,11 @@ int taille_liste(list *l)
     return taille;
 }
 
-list *renverse_liste(list *l)
+list *reverse_list(list *l)
 {
     // calculer la taille de la liste, puis mettre ses elements a l'envers dans un tableau
     // puis creer la liste a partir de ce tableau avec la fonction du prof.
-    int taille = taille_liste(l);
+    int taille = list_size(l);
     if (taille == 0)
     {
         printf("\nERREUR: la liste est vide.\n");
@@ -154,7 +154,7 @@ list *renverse_liste(list *l)
     return retour;
 }
 
-int valeur_a_la_position_liste(list *l, int i)
+int value_at_position(list *l, int i)
 {
     int indice = 0;
     while (l != NULL) // on balaye la liste
@@ -173,7 +173,7 @@ int valeur_a_la_position_liste(list *l, int i)
     return -1; // on est pas arrive a i car la liste s'est terminee avant
 }
 
-int position_de_la_premiere_valeur_liste(list *l, int valeur)
+int position_at_first_value(list *l, int valeur)
 {
     int indice = 0;
     while (l != NULL) // on balaye la liste
@@ -192,9 +192,9 @@ int position_de_la_premiere_valeur_liste(list *l, int valeur)
     return -1; // on est pas arrive a la valeur car la liste s'est terminee avant
 }
 
-int indice_minimum_liste(list *l)
+int position_of_minimum(list *l)
 {
-    int taille = taille_liste(l);
+    int taille = list_size(l);
     if (taille == 0)
     {
         printf("\nERREUR: la liste est vide.\n");
@@ -215,9 +215,9 @@ int indice_minimum_liste(list *l)
     return indice_minimum;
 }
 
-list *echange_dans_liste(list *l, int src, int dest)
+list *switch_into_list(list *l, int src, int dest)
 {
-    int taille = taille_liste(l);
+    int taille = list_size(l);
     if (taille == 0)
     {
         printf("\nERREUR: la liste est vide.\n");
@@ -244,9 +244,9 @@ list *echange_dans_liste(list *l, int src, int dest)
     return retour;
 }
 
-int indice_minimum_apres_liste(list *l, int apres)
+int position_of_minimum_after_a_position(list *l, int apres)
 {
-    int taille = taille_liste(l);
+    int taille = list_size(l);
     if (taille == 0)
     {
         printf("\nERREUR: la liste est vide.\n");
@@ -280,9 +280,9 @@ int indice_minimum_apres_liste(list *l, int apres)
     return indice_minimum;
 }
 
-list *trie_liste(list *l)
+list *order_list(list *l)
 {
-    int taille = taille_liste(l);
+    int taille = list_size(l);
     if (taille == 0)
     {
         printf("\nERREUR: la liste est vide.\n");
@@ -292,13 +292,13 @@ list *trie_liste(list *l)
     list *tri = l;
     for (int i = 0; i < taille; i++)
     {
-        tri = echange_dans_liste(tri, indice_minimum_apres_liste(tri, i), i);
+        tri = switch_into_list(tri, position_of_minimum_after_a_position(tri, i), i);
     }
     /*
-    printf("\nIndice minimum apres 0 : %d", indice_minimum_apres_liste(tri, 0));
-    printf("\nIndice minimum apres 4 : %d", indice_minimum_apres_liste(tri, 4));
-    printf("\nIndice minimum apres 9 : %d", indice_minimum_apres_liste(tri, 9));
-    printf("\nIndice minimum apres 10 : %d", indice_minimum_apres_liste(tri, 10));
+    printf("\nIndice minimum apres 0 : %d", position_of_minimum_after_a_position(tri, 0));
+    printf("\nIndice minimum apres 4 : %d", position_of_minimum_after_a_position(tri, 4));
+    printf("\nIndice minimum apres 9 : %d", position_of_minimum_after_a_position(tri, 9));
+    printf("\nIndice minimum apres 10 : %d", position_of_minimum_after_a_position(tri, 10));
 */
     return tri;
 }
@@ -313,7 +313,7 @@ list *add_to_list(list *l, int newhead)
 
 list *reach_member_list(list *l, int member)
 {
-    if (member > taille_liste(l) - 1)
+    if (member > list_size(l) - 1)
     {
         printf("\nERREUR: impossible d'atteindre l'element %d.\n", member);
         return NULL;
@@ -332,7 +332,7 @@ list *reach_member_list(list *l, int member)
 
 list *add_to_list_after_position(list *l, int newhead, int position)
 {
-    if (position > taille_liste(l))
+    if (position > list_size(l))
     {
         printf("\nERREUR: la position est hors limite, impossible d'inserer l'element en position %d.\n", position);
         return l;
@@ -340,6 +340,6 @@ list *add_to_list_after_position(list *l, int newhead, int position)
     // on ajoute le nouveau membre au debut
     list *newlist = add_to_list(l, newhead);
     // ensuite on permute avec la fonction adequate
-    newlist = echange_dans_liste(newlist, 0, position);
+    newlist = switch_into_list(newlist, 0, position);
     return newlist;
 }
