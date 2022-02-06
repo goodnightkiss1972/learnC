89f5df13079a9a08f4a954eff18601d73355a09b
goodnightkiss1972 <goodnightkiss1972@gmail.com>
2022-02-06 17:28:31 +0100

    passage de parametres nouvelles explications

--- variables_copie.c
+++ variables_copie.c
@@ -8,13 +8,14 @@ int main() {
     printf("Avant la fonction, i a pour adresse %x et valeur %d.\n", &i, i);
     // exemple de passage par copie
     fonction(i);
-    printf("Apres la fonction, i a pour adresse %x et valeur %d.\n", &i, i);
+    printf("Apres l'appel i a pour adresse %x et valeur %d.\n", &i, i);
+    printf("Le parametre n'a pas ete modifie pour l'appelant, on en a fait une copie.\n");
     return 0;
 }
 
 void fonction(int fi) {
-    printf("Dans la fonction, fi a pour adresse %x et valeur %d.\n",&fi, fi);
+    printf("En entrant dans la fonction, le parametre recu a pour adresse %x et valeur %d.\n",&fi, fi);
     fi = fi +1;
-    printf("Dans la fonction, fi a pour adresse %x et valeur %d.\n",&fi, fi);
+    printf("Apres i+1 dans la fonction,  le parametre recu a pour adresse %x et valeur %d.\n",&fi, fi);
     return;
 }
--- variables_reference.c
+++ variables_reference.c
@@ -8,13 +8,14 @@ int main() {
     printf("Avant la fonction, i a pour adresse %x et valeur %d.\n", &i, i);
     // exemple de passage par reference
     fonction(ptri); // on aurait aussi pu appeler avec fonction(&i);
-    printf("Apres la fonction, i a pour adresse %x et valeur %d.\n", &i, i);
+    printf("Apres l'appel i a pour adresse %x et valeur %d.\n", &i, i);
+    printf("le parametre a ete modifie dans la fonction (passage par reference)\n");
     return 0;
 }
 
 void fonction(int *fi) {
-    printf("Dans la fonction, fi a pour adresse %x et valeur %d.\n",&fi, *fi);
+    printf("En entrant dans la fonction, le parametre recu a pour adresse %x et valeur %d.\n",&fi, *fi);
     *fi = *fi +1;
-    printf("Dans la fonction, fi a pour adresse %x et valeur %d.\n",&fi, *fi);
+    printf("Apres i+1 dans la fonction,  le parametre recu a pour adresse %x et valeur %d.\n",&fi, *fi);
     return;
 }
